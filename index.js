442cf637c6a52414d1c3b91e08a9cba9b2aa0bdb
Muhammad Abid Dewantoro <151120880+DeadEye27@users.noreply.github.com>
2026-04-25 16:45:20 +0700

    feat: setup mysql connection using mysql2 pool

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+const db = require('./config/database');
+
 app.use(cors());
 app.use(express.json());
 
