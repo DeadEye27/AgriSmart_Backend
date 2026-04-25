55af62b73f9482aa60cb694e793aade278ebcc2c
Muhammad Abid Dewantoro <151120880+DeadEye27@users.noreply.github.com>
2026-04-25 15:41:54 +0700

    feat: setup basic express server and middlewares

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors =  require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+        message: "Halo dari Backend AgriSmart! Server berjalan dengan baik",
+    });
+});
+
+app.listen(port, ()=> {
+    console.log(`[SERVER] AgriSmart API berjalan di http://localhost:${port}`);
+});
